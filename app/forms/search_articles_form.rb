1b11df78d56d99697c9a6695612205df72594e85
大村純平 <junpei.aug07@gmail.com>
2021-02-17 23:31:16 +0900

    Add: 著者、タグ、記事内容に関しても検索が行えるようにした

--- app/forms/search_articles_form.rb
+++ app/forms/search_articles_form.rb
@@ -4,14 +4,27 @@ class SearchArticlesForm
 
   attribute :category_id, :integer
   attribute :title, :string
+  attribute :author_id, :integer
+  attribute :body, :string
+  attribute :tag_id, :integer
 
   def search
     relation = Article.distinct
 
     relation = relation.by_category(category_id) if category_id.present?
+
+    relation = relation.by_author(author_id) if author_id.present?
+
+    relation = relation.by_tag(tag_id) if tag_id.present?
+
     title_words.each do |word|
       relation = relation.title_contain(word)
     end
+
+    body_words.each do |word|
+      relation = relation.body_contain(word)
+    end
+
     relation
   end
 
@@ -19,5 +32,9 @@ class SearchArticlesForm
 
   def title_words
     title.present? ? title.split(nil) : []
+  end
+
+  def body_words
+    body.present? ? body.split(nil) : []
   end
 end
